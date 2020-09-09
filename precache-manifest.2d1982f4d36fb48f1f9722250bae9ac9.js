7d1e0b5eb48ca281f26769c9293d1cc84c700e80
opv1 <pas7273@gmail.com>
2020-09-09 13:03:56 +0300

    Updates

---
+++ precache-manifest.2d1982f4d36fb48f1f9722250bae9ac9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48f424cedce0ef62b1448927bb4184e",
+    "url": "/github-repo/index.html"
+  },
+  {
+    "revision": "4093708ba3af1de4eec0",
+    "url": "/github-repo/static/css/main.fc46c68a.chunk.css"
+  },
+  {
+    "revision": "b14e9dcb3deeced3fb81",
+    "url": "/github-repo/static/js/2.a3a39e73.chunk.js"
+  },
+  {
+    "revision": "a1a88636b752ef668fe9699df3c2d360",
+    "url": "/github-repo/static/js/2.a3a39e73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4093708ba3af1de4eec0",
+    "url": "/github-repo/static/js/main.fed79845.chunk.js"
+  },
+  {
+    "revision": "ef8b6aa2c7a2f541a7b9",
+    "url": "/github-repo/static/js/runtime-main.4aac5d58.js"
+  }
+]);
