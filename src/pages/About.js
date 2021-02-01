8a50bb4ea3b8a1e54ac868bd61993df511e2636a
opv1 <pas7273@gmail.com>
2021-02-01 23:02:58 +0300

    more beauty

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,9 +4,9 @@ export const About = () => {
   return (
     <div className='jumbotron'>
       <div className='container'>
-        <h1 className='display-4'>Информация</h1>
+        <h1 className='display-5'>Информация</h1>
         <p className='lead'>
-          Версия приложения: <strong>1.0.0</strong>
+          Версия приложения: <strong>0.1.0</strong>
         </p>
       </div>
     </div>
